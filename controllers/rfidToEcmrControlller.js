28190c6fc7c83a339c647c873cd8bc84c9d467af
Dries Geenen <driesgeenen@gmail.com>
2018-02-20 13:54:12 +0100

    idk

--- controllers/rfidToEcmrControlller.js
+++ controllers/rfidToEcmrControlller.js
@@ -5,6 +5,8 @@ var jwt = require('jsonwebtoken');
 var request = require('request-promise');
 
 var orderDataServiceUrl = process.env.SCAN_SERVICE_URL || 'http://localhost:6602/orders';
+var configServiceUrl = process.env.CONFIG_SERVICE_URL || 'http://localhost:6610/config';
+var ecmrServiceUrl = process.env.ECMR_SERVICE_URL || 'http://localhost:6603/ecmrs';
 var printServiceUrl = process.env.PRINT_SERVICE_URL || 'http://localhost:6607/print';
 
 exports.buildEcmr = function (req, res) {
@@ -16,12 +18,48 @@ exports.buildEcmr = function (req, res) {
         productCode: String
     }]
     */
+
+    var orders;
+    var ecmrs;
+
     var promise = requestOrderInformation(freightInformation.products);
     promise.then(function (response) {
-        var orders = JSON.parse(response).data;
-        return res.json({success: true, data: orders});
+        orders = JSON.parse(response).data;
+        /*
+        [{
+            _id: String,
+            sender: Contact,
+            receiver: Contact,
+            goods: [{
+                "productCode": String,
+                "name": String,
+                "amount": Number
+            }]
+         }]
+         */
+        return requestConfiguration();
     }, function (err) {
         return res.status(500).json({success: false, msg: 'Failed to get orders', error: err});
+    }).then(function (response) {
+        var configuration = JSON.parse(response).data;
+        /*
+        mainTransporter: Contact,
+        autographs:{
+            transporter: String
+        }
+        */
+        ecmrs = getEcmrs(orders, configuration, freightInformation.driver);
+        return requestSaveEcmrs(ecmrs);
+    }, function (err) {
+        return res.status(500).json({success: false, msg: 'Failed to get configuration', error: err});
+    }).then(function () {
+        return requestPrintCmrs();
+    }, function (err) {
+        return res.status(500).json({success: false, msg: 'Failed to save ecmrs', error: err});
+    }).then(function () {
+        return res.status(500).json({success: true, msg: 'Ecmrs have been saved and printed'});
+    }, function (err) {
+        return res.status(500).json({success: false, msg: 'Failed to print ecmrs', error: err});
     });
 };
 
@@ -37,10 +75,79 @@ const requestOrderInformation = function (productArray) {
     return request.post(options);
 };
 
-var generateRfidToCmrServiceToken = function () {
+const requestConfiguration = function () {
+    var options = {
+        url: configServiceUrl,
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': generateRfidToCmrServiceToken()
+        }
+    };
+    return request(options);
+};
+
+const requestSaveEcmrs = function (ecmrs) {
+    var options = {
+        url: ecmrServiceUrl + '/many',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': generateRfidToCmrServiceToken()
+        },
+        body: JSON.stringify(ecmrs)
+    };
+    return request.post(options);
+};
+
+const requestPrintCmrs = function (ecmrs) {
+    var options = {
+        url: printServiceUrl,
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': generateRfidToCmrServiceToken()
+        },
+        body: JSON.stringify(ecmrs)
+    };
+    return request.post(options);
+};
+
+const generateRfidToCmrServiceToken = function () {
     return 'JWT ' + jwt.sign({
         data: {role: 'rfidtocmrservice'}
     }, config.secret, {
         expiresIn: 60
     });
+};
+
+const getEcmrs = function (orders, config, driver) {
+    var ecmrs = [];
+
+    for (var i = 0; i < orders.length; i++) {
+        ecmrs.push(getEcmr(orders[i], config, driver));
+    }
+
+    return ecmrs;
+};
+
+const getEcmr = function (order, config, driver) {
+    return {
+        driver: driver,
+        setup: {
+            location: config.mainTransporter.location, // Dit zou het magazijn moeten zijn in een real-world situatie
+            time: new Date()
+        },
+        sender: order.sender,
+        receiver: order.receiver,
+        deliveryLocation: order.receiver.location,  // factuuradres === leveradres
+        mainTransporter: config.mainTransporter,
+        reception: {
+            location: order.receiver.location
+        },
+        goods: order.goods,
+        confirmedDelivery: undefined,
+        autographs: {
+            sender: undefined, // Zou uit ordersysteem kunnen komen... mischien.
+            receiver: undefined,
+            transporter: config.autographs.transporter
+        }
+    }
 };
